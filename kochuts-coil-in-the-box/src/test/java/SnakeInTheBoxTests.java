2cdf417d29fdbf50de42d3c508eec5992517a005
crin <crinuyi@gmail.com>
2021-05-15 22:26:01 +0200

    renamed: HypercubeTests.java -> SnakeInTheBoxTests.java

--- kochuts-coil-in-the-box/src/test/java/SnakeInTheBoxTests.java
+++ kochuts-coil-in-the-box/src/test/java/SnakeInTheBoxTests.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class HypercubeTests {
+public class SnakeInTheBoxTests {
     private Hypercube hypercube;
     private SnakeInTheBox snakeInTheBox;
 
